9fb368d600d922b94cd908e0cd56f134377b70cc
parsa <parsa.seidahmad@gmail.com>
2021-06-01 21:46:49 +0430

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+const App:any=()=> {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <button>+ task</button>
+      </header>
+      <body><p>body</p></body>
+    </div>
+  );
+}
+
+export default App;
